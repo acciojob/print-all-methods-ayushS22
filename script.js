1b693f0678c9059e0f4c21a1bb76e660cb3f55bc
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2026-01-09 15:10:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-//your JS code here. If required.
 function allMethods() {
-  //write your code here
+  return Object.getOwnPropertyNames(Math)
+    .filter(prop => typeof Math[prop] === "function")
+    .join(", ");
 }
 
 alert(allMethods());
